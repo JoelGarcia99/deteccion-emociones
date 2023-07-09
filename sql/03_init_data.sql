e104e8450a9ccc413037a75828b7fee15d6f297c
Joel Garcia <garciajunior796@gmail.com>
2023-07-09 13:20:53 -0500

    SQL init resources data

---
+++ sql/03_init_data.sql
@@ -0,0 +1,28 @@
+INSERT INTO public."Recurso" (id,nombre,tipo,url,embebido,proposito) VALUES
+	 ('187e76d7-61e4-471e-8892-109289e8a3b5','Number Maze','videogame',NULL,'https://cdn.htmlgames.com/NumberMaze/','neutral'),
+	 ('070ca376-fa39-47b6-8ccc-de3d7a67f766','Honey Bee','videogame',NULL,'https://cdn.htmlgames.com/HoneyBee/','neutral'),
+	 ('8847b7cc-b1b5-4853-bb4f-1ceb0c80a09d','Easter Pile','videogame',NULL,'https://cdn.htmlgames.com/EasterPile/','neutral'),
+	 ('9be3420e-0079-4069-934a-99268b12ebb2','Toy Factory','videogame',NULL,'https://cdn.htmlgames.com/ToyFactory/','neutral'),
+	 ('d807e932-7a88-43f1-a324-526d84668c03','Frozen for Christmas','videogame',NULL,'https://cdn.htmlgames.com/FrozenForChristmas/','angry'),
+	 ('348b0753-b12d-4e1c-8de0-4fcee328aced','Zoo Misteries','videogame',NULL,'https://cdn.htmlgames.com/ZooMysteries/','angry'),
+	 ('5eb31674-857c-4b71-8c24-3c86d5e0e562','Bowling','videogame',NULL,'https://cdn.htmlgames.com/Bowling/','angry'),
+	 ('ed36b57b-7914-4bb8-b3f0-8316e17a6b57','Wizard Jewels','videogame',NULL,'https://cdn.htmlgames.com/WizardJewels/','surprise'),
+	 ('901b2b2e-b734-481a-9b37-f9a137533a46','Monkey on Problem','videogame',NULL,'https://cdn.htmlgames.com/MonkeyInTrouble/','happy'),
+	 ('1a2b2f7a-1e5f-4137-9372-f7eaa415179f','Toy Factory','videogame',NULL,'https://cdn.htmlgames.com/ToyFactory/','happy');
+INSERT INTO public."Recurso" (id,nombre,tipo,url,embebido,proposito) VALUES
+	 ('45ae4c1e-16a1-4287-be5b-bc9663e70f2c','Minesweeper','videogame',NULL,'https://cdn.htmlgames.com/Minesweeper/','happy'),
+	 ('f22229a2-dfae-467f-af38-8d69efed95ad','Block Monster 1010','videogame',NULL,'https://cdn.htmlgames.com/BlockMonsters1010/','happy'),
+	 ('35b11b0c-c146-4155-a468-3c60a4150440','Plushy Animals','videogame',NULL,'https://cdn.htmlgames.com/PlushyAnimals/','happy'),
+	 ('fd0e1a0a-f9b4-43e0-8823-15c30179b5d7','Greedy Worm','videogame',NULL,'https://cdn.htmlgames.com/GreedyWorm/','sad'),
+	 ('9fa2857c-a4e2-4174-a50d-44aeb61eb32b','Classic Snake','videogame',NULL,'https://cdn.htmlgames.com/ClassicSnake/','sad'),
+	 ('33544a9f-2bd0-4f60-bf50-a4c66d9e0cea','Wizard Hewels','videogame',NULL,'https://cdn.htmlgames.com/WizardJewels/','sad'),
+	 ('d3c17b8d-fd0a-4f30-bc1e-39b585889b3f','Classic Snake','videogame',NULL,'https://cdn.htmlgames.com/ClassicSnake/','sad'),
+	 ('c42a511f-20a7-4e44-b891-53aade501f36','Dark Connect','videogame',NULL,'https://cdn.htmlgames.com/DarkMahjongConnect/','sad'),
+	 ('fe7b972e-1238-46c4-a973-8d6580653386','Greedy Worm','videogame',NULL,'https://cdn.htmlgames.com/GreedyWorm/','fear'),
+	 ('ac2d4531-b662-44e5-aca6-525ecb5561e9','Greedy Worm','videogame',NULL,'https://cdn.htmlgames.com/GreedyWorm/','surprise');
+INSERT INTO public."Recurso" (id,nombre,tipo,url,embebido,proposito) VALUES
+	 ('500727a3-b970-4df2-9678-66de853b14ec','Classic Snake','videogame',NULL,'https://cdn.htmlgames.com/ClassicSnake/','surprise'),
+	 ('f86f64c7-d2b7-4577-8d37-23ae9e3aff70','Classic Snake','videogame',NULL,'https://cdn.htmlgames.com/ClassicSnake/','fear'),
+	 ('bb216ad8-6a3f-4266-8a8d-4854936291dc','Wizard Jewels','videogame',NULL,'https://cdn.htmlgames.com/WizardJewels/','fear'),
+	 ('d4aa2ea1-7808-4ac8-b6fa-bbd8a4041cf6','El ahorcado','videogame',NULL,'https://cdn.htmlgames.com/Hangman/','neutral'),
+	 ('d4fe0393-4e2b-4164-b86c-9d747e854bf8','Forest Bubbles','videogame',NULL,'https://cdn.htmlgames.com/ForestBubbles/','angry');
